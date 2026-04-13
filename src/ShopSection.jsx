59b6f48119baa1b893df2587bd34fa78c1a95301
satyam779 <satyamreddy7674@gmail.com>
2026-04-13 12:37:43 +0530

    src/ to ./assets

--- src/ShopSection.jsx
+++ src/ShopSection.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import DIYKIT from '/src/assets/DIY Educational Kits.jpg'
-import VirtualLearningImage from '/src/assets/virtuallearningCoursesImage.jpg'
-import StemWorkShopImage from '/src/assets/STEM Workshop - 4.jpg'
+import DIYKIT from './assets/DIY Educational Kits.jpg'
+import VirtualLearningImage from './assets/virtuallearningCoursesImage.jpg'
+import StemWorkShopImage from './assets/STEM Workshop - 4.jpg'
 import './ShopSection.css';
 
 function ShopSection(){
